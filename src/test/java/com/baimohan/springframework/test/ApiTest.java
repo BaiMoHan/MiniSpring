640184eed46c8630edd083f7f35405083a0723bf
baimohan <csbaimohan@foxmail.com>
2021-07-24 19:04:06 +0800

    更新测试用例

--- src/test/java/com/baimohan/springframework/test/ApiTest.java
+++ src/test/java/com/baimohan/springframework/test/ApiTest.java
@@ -1,7 +1,7 @@
 package com.baimohan.springframework.test;
 
-import com.baimohan.springframework.BeanDefinition;
-import com.baimohan.springframework.BeanFactory;
+import com.baimohan.springframework.beans.factory.config.BeanDefinition;
+import com.baimohan.springframework.beans.factory.support.DefaultListableBeanFactory;
 import com.baimohan.springframework.test.bean.UserService;
 import org.junit.Test;
 
@@ -20,14 +20,18 @@ public class ApiTest {
     @Test
     public void test_BeanFactory(){
         // 1.初始化 BeanFactory
-        BeanFactory beanFactory = new BeanFactory();
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
 
         // 2.注入bean
-        BeanDefinition beanDefinition = new BeanDefinition(new UserService());
+        BeanDefinition beanDefinition = new BeanDefinition(UserService.class);
         beanFactory.registerBeanDefinition("userService", beanDefinition);
 
-        // 3.获取bean
+        // 3.第一次获取bean
         UserService userService = (UserService) beanFactory.getBean("userService");
         userService.queryUserInfo();
+
+        // 3.第二次获取bean
+        UserService userService2 = (UserService) beanFactory.getBean("userService");
+        userService2.queryUserInfo();
     }
 }
